9ad35ac826c8e3ddf65be95b2162bf31c5e1814d
Evgenii Chertin <125874750+Eugeny2010@users.noreply.github.com>
2023-06-22 17:59:21 +0400

    add map
    add map

---
+++ flags.js
@@ -0,0 +1,19 @@
+//var id = '<img src="flags/" height="200px" width="300px"/>';
+
+var asri = '<img src="flags/asr.jpg" height="200px" width="300px"/>';
+var ruri = '<img src="flags/rur.jpg" height="200px" width="300px"/>';
+var voli = '<img src="flags/vol.jpg" height="200px" width="300px"/>';
+var liti = '<img src="flags/lit.jpg" height="200px" width="300px"/>';
+var rski = '<img src="flags/rsk.jpg" height="200px" width="300px"/>';
+var sevi = '<img src="flags/sev.jpg" height="200px" width="300px"/>';
+var reti = '<img src="flags/ret.jpg" height="200px" width="300px"/>';
+var slai = '<img src="flags/sla.jpg" height="200px" width="300px"/>';
+var ruli = '<img src="flags/rul.jpg" height="200px" width="300px"/>';
+var kapi = '<img src="flags/kap.jpg" height="200px" width="300px"/>';
+var akri = '<img src="flags/akr.jpg" height="200px" width="300px"/>';
+var sori = '<img src="flags/sor.jpg" height="200px" width="300px"/>';
+var viki = '<img src="flags/vik.jpg" height="200px" width="300px"/>';
+var lyui = '<img src="flags/lyu.jpg" height="200px" width="300px"/>';
+var stri = '<img src="flags/str.jpg" height="200px" width="300px"/>';
+var ensi = '<img src="https://sun9-22.userapi.com/impg/lhxMiswXjRH0H7VGP_FZ3bOxEEhQWR4Bn7orcA/eLky_JT_qFg.jpg?size=1280x853&quality=95&sign=de5c93c7e01b0c9c0a97b3ed224672ce&type=album" height="200px" width="300px"/>';
+var huri = '<img src="https://sun9-68.userapi.com/impg/lPwOVbp3M6NNAFnzYuZpgVcuIo56uvARHbXpRw/8YocsHp6fC4.jpg?size=590x393&quality=95&sign=ef7826f76d86dfd9cdc69da060d4217f&type=album" height="200px" width="300px"/>';
